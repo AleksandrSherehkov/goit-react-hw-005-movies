3595e1caef8f71f6130b805729becc5497030ced
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-06-26 13:34:07 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,11 +1,11 @@
-import { Modal } from 'components/Modal/Modal';
+// import { Modal } from 'components/Modal/Modal';
 import { ImageGalleryItemImage, ImageGalleryItemStyled } from './ImageGalleryItem.styled';
 
 export const ImageGalleryItem = ({ fotos }) => {
   return fotos.map(({ id, webformatURL, tags }) => (
     <ImageGalleryItemStyled key={id}>
       <ImageGalleryItemImage src={webformatURL} alt={tags} />
-      <Modal />
+      {/* <Modal /> */}
     </ImageGalleryItemStyled>
   ));
 };
