fc09916a9d19b45aefacb79f97c238eff00ea14a
Aleksandr Shereshkov <aleksandr.shereshkov@gmail.com>
2023-07-02 20:21:29 +0300

    update

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,21 @@
+import PropTypes from 'prop-types';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { ImageGalleryStyled } from './ImageGallery.styled';
 
 export const ImageGallery = ({ fotos }) => {
   return (
-    <>
-      <ImageGalleryStyled>
-        {fotos.map(item => (
-          <ImageGalleryItem key={item.id} {...item} />
-        ))}
-      </ImageGalleryStyled>
-    </>
+    <ImageGalleryStyled>
+      {fotos.map(item => (
+        <ImageGalleryItem key={item.id} {...item} />
+      ))}
+    </ImageGalleryStyled>
   );
+};
+
+ImageGallery.propTypes = {
+  fotos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { ProgressBar } from 'react-loader-spinner';
 import { Flex } from './Loader.styled';
 
