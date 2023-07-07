329fda261b7d0d209d1273acc1cc6e4676a906ea
AleksandrSherehkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2023-07-07 00:03:02 +0000

    Deploying to gh-pages from @ AleksandrSherehkov/goit-react-hw-005-movies@db07d55c98725e9109767452ae28dc392721bc4f 🚀

---
+++ static/js/276.20cff861.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[276],{4276:function(e,t,n){n.r(t);n(2791);var r=n(184);t.default=function(){return(0,r.jsxs)("main",{style:{textAlign:"center"},children:[(0,r.jsx)("b",{style:{fontSize:64},children:"404"}),(0,r.jsx)("p",{children:"Sorry, we couldn't find that page :("})]})}}}]);
+//# sourceMappingURL=276.20cff861.chunk.js.map
